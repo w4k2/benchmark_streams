053a0934c8e7c3dce5cf2fd24f59430faa1ae239
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2023-10-30 14:08:42 +0100

    Five years had gone like just one day. Removing legacy SL validation and extending streams to one million samples.

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import helper as h
-import strlearn as sl
+# import strlearn as sl
 import matplotlib.pyplot as plt
 
 """
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 7 attributes
 2 classes
 """
-length = 100000
+length = 1000000
 path = "datasets/%s"
 generators = {
     "s_hyp_r1": "generators.HyperplaneGenerator -a 7 -i 1 -n 5",
@@ -18,25 +18,29 @@ generators = {
     "s_led_r3": "generators.LEDGenerator -i 3 -n 15",
     "s_rbf_r1": "generators.RandomRBFGenerator -n 25 -a 7 -r 1 -i 1",
     "s_rbf_r2": "generators.RandomRBFGenerator -n 50 -a 7 -r 2 -i 2",
-    "s_rbf_r3": "generators.RandomRBFGenerator -n 100 -a 7 -r 3 -i 3"
+    "s_rbf_r3": "generators.RandomRBFGenerator -n 100 -a 7 -r 3 -i 3",
+    "s_sea_r1": "generators.SEAGenerator -f 1 -i 3",
+    "s_sea_r2": "generators.SEAGenerator -f 2 -i 2",
+    "s_sea_r3": "generators.SEAGenerator -f 3 -i 1"
 }
 drift_pairs = [
     ["s_hyp_r1","s_hyp_r2"], ["s_hyp_r2","s_hyp_r3"], ["s_hyp_r1","s_hyp_r3"],
     ["s_led_r1","s_led_r2"], ["s_led_r2","s_led_r3"], ["s_led_r1","s_led_r3"],
     ["s_rbf_r1","s_rbf_r2"], ["s_rbf_r2","s_rbf_r3"], ["s_rbf_r1","s_rbf_r3"],
-    ["s_hyp_r1","s_rbf_r1"], ["s_hyp_r2","s_rbf_r2"], ["s_hyp_r3","s_rbf_r3"]
+    ["s_hyp_r1","s_rbf_r1"], ["s_hyp_r2","s_rbf_r2"], ["s_hyp_r3","s_rbf_r3"],
+    ["s_sea_r1","s_sea_r2"], ["s_sea_r2","s_sea_r3"], ["s_sea_r1","s_sea_r3"]
 ]
 
-def test(dbname):
-    X, y = sl.utils.load_arff(path % "%s.arff" % dbname )
-    learner = sl.Learner(X,y,chunk_size=100,evaluate_interval=1000)
-    learner.run()
-    print(learner.scores)
-    plt.plot(learner.score_points,learner.scores)
-    plt.title(dbname)
-    plt.ylim([0,1])
-    plt.savefig("figures/%s.png" % dbname)
-    plt.clf()
+# def test(dbname):
+#     X, y = sl.utils.load_arff(path % "%s.arff" % dbname )
+#     learner = sl.Learner(X,y,chunk_size=100,evaluate_interval=1000)
+#     learner.run()
+#     print(learner.scores)
+#     plt.plot(learner.score_points,learner.scores)
+#     plt.title(dbname)
+#     plt.ylim([0,1])
+#     plt.savefig("figures/%s.png" % dbname)
+#     plt.clf()
 
 """
 1. Stationary streams
@@ -46,7 +50,7 @@ for key in generators:
     generator = generators[key]
     print(generator, key)
     h.generate_stream(path % key, length, window, [generator])
-    test(key)
+    # test(key)
 
 """
 2. Sudden drifts
@@ -59,7 +63,7 @@ for drift_pair in drift_pairs:
     filename = "sd_%s_%s" % (first, second)
     print(filename)
     h.generate_stream(path % filename, length, window, streams)
-    test(filename)
+    # test(filename)
 
 """
 3. Incremental drifts
@@ -72,5 +76,5 @@ for drift_pair in drift_pairs:
     filename = "id_%s_%s" % (first, second)
     print(filename)
     h.generate_stream(path % filename, length, window, streams)
-    test(filename)
+    # test(filename)
     
